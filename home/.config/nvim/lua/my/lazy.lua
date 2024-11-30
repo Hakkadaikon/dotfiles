5955019251fff6ad4b216dc54aa16a84b102b3e2
Hakkadaikon <hakkadaikon@yahoo.co.jp>
2024-11-30 15:05:47 +0900

    Move package manager from vim-plug to lazy.nvim.

---
+++ home/.config/nvim/lua/my/lazy.lua
@@ -0,0 +1,63 @@
+local mylazy = {}
+
+function mylazy.setup()
+  -- Install package manager
+  --    https://github.com/folke/lazy.nvim
+  --    `:help lazy.nvim.txt` for more info
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+
+  vim.opt.rtp:prepend(lazypath)
+
+  local opts = {
+    defaults = {
+      lazy = false,
+    },
+    performance = {
+      cache = {
+        enabled = true,
+      },
+    },
+  }
+
+  -- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be automatically merged in the main plugin spec
+  -- require('lazy').setup('plugins', opts)
+  require('lazy').setup({
+    'neovim/nvim-lspconfig',
+    'scrooloose/nerdcommenter',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/vim-vsnip',
+    'obaland/vfiler.vim',
+    'machakann/vim-highlightedyank',
+    'nvim-lualine/lualine.nvim',
+    'kamykn/spelunker.vim',
+    'mhinz/vim-startify',
+    'bluz71/vim-nightfly-guicolors',
+    'marko-cerovac/material.nvim',
+    'ConradIrwin/vim-bracketed-paste',
+    'kana/vim-smartinput',
+    'airblade/vim-gitgutter',
+    'tpope/vim-fugitive',
+    'scrooloose/vim-slumlord',
+    'thinca/vim-quickrun',
+    'haya14busa/vim-edgemotion',
+    'kana/vim-smartword',
+    'tani/dmacro.vim',
+    'vim-denops/denops.vim',
+    'vim-fall/fall.vim',
+  })
+end
+
+return mylazy
