3de738d4cf1ce52ee58de4a941160046bfd7cfe5
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-05-15 19:32:24 +0900

    luaformatter -> stylua

--- home/.config/nvim/lua/my/lsp.lua
+++ home/.config/nvim/lua/my/lsp.lua
@@ -17,45 +17,61 @@ function mylsp.setup()
 
   lspconfig.terraformls.setup({})
 
-  lspconfig.clangd.setup(
-      {
-        cmd = {"clangd", "--background-index", "--clang-tidy", "--completion-style=detailed"},
-        settings = {clangd = {fallbackFlags = {"-std=c++17"}}},
-      }
-  )
+  lspconfig.clangd.setup({
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--completion-style=detailed",
+    },
+    settings = {
+      clangd = {
+        fallbackFlags = {
+          "-std=c++17",
+        },
+      },
+    },
+  })
 
-  lspconfig.intelephense.setup({settings = {intelephense = {environment = {phpVersion = "8.2"}}}})
+  lspconfig.intelephense.setup({
+    settings = {
+      intelephense = {
+        environment = {
+          phpVersion = "8.2",
+        },
+      },
+    },
+  })
 
   -- Mason
   require("mason").setup({})
-  require("mason-lspconfig").setup_handlers(
-      {
-        function(server)
-          local opt = {
-            capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-          }
-          require("lspconfig")[server].setup(opt)
-        end,
+  require("mason-lspconfig").setup_handlers({
+    function(server)
+      local opt = {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
       }
-  )
+      require("lspconfig")[server].setup(opt)
+    end,
+  })
 
-  lspconfig.denols.setup(
-      {
-        root_dir = lspconfig.util.root_pattern("deno.json"),
-        init_options = {
-          lint = true,
-          unstable = true,
-          suggest = {
-            imports = {
-              hosts = {["https://deno.land"] = true, ["https://cdn.nest.land"] = true, ["https://crux.land"] = true},
-            },
+  lspconfig.denols.setup({
+    root_dir = lspconfig.util.root_pattern("deno.json"),
+    init_options = {
+      lint = true,
+      unstable = true,
+      suggest = {
+        imports = {
+          hosts = {
+            ["https://deno.land"] = true,
+            ["https://cdn.nest.land"] = true,
+            ["https://crux.land"] = true,
           },
         },
-      }
-  )
+      },
+    },
+  })
 
-  vim.cmd(
-      [[
+  vim.cmd([[
   set updatetime=500
   highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
   highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
@@ -65,30 +81,25 @@ function mylsp.setup()
     "autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
     "autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
   augroup END
-  ]]
-  )
+  ]])
 
   local cmp = require("cmp")
-  cmp.setup(
-      {
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        sources = {{name = "nvim_lsp"}, {name = "buffer"}, {name = "path"}},
-        mapping = cmp.mapping.preset.insert(
-            {
-              ["<C-p>"] = cmp.mapping.select_prev_item(),
-              ["<C-n>"] = cmp.mapping.select_next_item(),
-              ["<C-l>"] = cmp.mapping.complete(),
-              ["<C-e>"] = cmp.mapping.abort(),
-              ["<CR>"] = cmp.mapping.confirm({select = true}),
-            }
-        ),
-        experimental = {ghost_text = true},
-      }
-  )
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    sources = { { name = "nvim_lsp" }, { name = "buffer" }, { name = "path" } },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-l>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    }),
+    experimental = { ghost_text = true },
+  })
 end
 
 return mylsp
