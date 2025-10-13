127cec4339fa5aab0d27d41ff4498be6f7e9f283
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-10-13 21:46:37 +0900

    Organize code. Moved tiny code config

--- home/.config/nvim/lua/my/color.lua
+++ home/.config/nvim/lua/my/color.lua
@@ -4,6 +4,8 @@ function mycolor.setup()
   vim.opt.termguicolors = true -- Enable terminal GUI colors
   vim.opt.winblend = 0
   vim.opt.pumblend = 0
+
+  vim.api.nvim_set_hl(0, "LineAdove", { fg = "#ffe5b4" })
 end
 
 return mycolor
---
+++ home/.config/nvim/lua/my/lib/tiny_code.lua
@@ -0,0 +1,82 @@
+M = {}
+
+M.diagnostic_config = {
+  preset = "modern",
+  transparent_bg = false,
+  hi = {
+    error = "DiagnosticError",
+    warn = "DiagnosticWarn",
+    info = "DiagnosticInfo",
+    hint = "DiagnosticHint",
+    arrow = "NonText",
+    background = "CursorLine",
+    mixing_color = "None",
+  },
+
+  options = {
+    show_source = false,
+    use_icons_from_diagnostic = false,
+    set_arrow_to_diag_color = false,
+    add_messages = true,
+    throttle = 20,
+    softwrap = 30,
+    multilines = { enabled = false, always_show = false },
+    show_all_diags_on_cursorline = false,
+    enable_on_insert = false,
+    enable_on_select = false,
+    overflow = { mode = "wrap", padding = 0 },
+    break_line = { enabled = false, after = 30 },
+    -- Custom format function for diagnostic messages
+    -- Example:
+    -- format = function(diagnostic)
+    --     return diagnostic.message .. " [" .. diagnostic.source .. "]"
+    -- end
+    format = nil,
+    virt_texts = { priority = 2048 },
+    severity = {
+      vim.diagnostic.severity.ERROR,
+      vim.diagnostic.severity.WARN,
+      vim.diagnostic.severity.INFO,
+      vim.diagnostic.severity.HINT,
+    },
+    overwrite_events = nil,
+  },
+  disabled_ft = {}, -- List of filetypes to disable the plugin
+}
+
+M.action_config = {
+  backend = "vim",
+  backend_opts = {
+    delta = { header_lines_to_remove = 4, args = { "--line-numbers" } },
+    difftastic = {
+      header_lines_to_remove = 1,
+      args = { "--color=always", "--display=inline", "--syntax-highlight=on" },
+    },
+  },
+  telescope_opts = {
+    layout_strategy = "vertical",
+    layout_config = {
+      width = 0.7,
+      height = 0.9,
+      preview_cutoff = 1,
+      preview_height = function(_, _, max_lines)
+        local h = math.floor(max_lines * 0.5)
+        return math.max(h, 10)
+      end,
+    },
+  },
+  signs = {
+    quickfix = { "󰁨", { link = "DiagnosticInfo" } },
+    others = { "?", { link = "DiagnosticWarning" } },
+    refactor = { "", { link = "DiagnosticWarning" } },
+    ["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
+    ["refactor.extract"] = { "", { link = "DiagnosticError" } },
+    ["source.organizeImports"] = { "", { link = "TelescopeResultVariable" } },
+    ["source.fixAll"] = { "", { link = "TelescopeResultVariable" } },
+    ["source"] = { "", { link = "DiagnosticError" } },
+    ["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
+    ["codeAction"] = { "", { link = "DiagnosticError" } },
+  },
+}
+
+return M
