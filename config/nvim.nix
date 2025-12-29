218d4e940a9fb9adde16e6b49cbaf7a89171082d
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-12-29 16:01:55 +0900

    refactor: Separate nix file

---
+++ config/nvim.nix
@@ -0,0 +1,14 @@
+final: prev: {
+  neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (oldAttrs: {
+    version = "0.11.5";
+    nvimVersion = "0.11.5";
+    nvimHash = "sha256-OsvLB9kynCbQ8PDQ2VQ+L56iy7pZ0ZP69J2cEG8Ad8A="; 
+
+    src = prev.fetchFromGitHub {
+      owner = "neovim";
+      repo = "neovim";
+      rev = "v0.11.5";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; 
+    };
+  });
+}
--- flake.nix
+++ flake.nix
@@ -6,38 +6,31 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        nvimVersion = "0.11.5";
-        nvimHash = "sha256-OsvLB9kynCbQ8PDQ2VQ+L56iy7pZ0ZP69J2cEG8Ad8A="; 
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
 
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (oldAttrs: {
-                version = nvimVersion;
-                src = prev.fetchFromGitHub {
-                  owner = "neovim";
-                  repo = "neovim";
-                  rev = "v${nvimVersion}";
-                  hash = nvimHash;
-                };
-              });
-            })
-          ];
-        };
-      in
-      {
-        packages = {
-          default = pkgs.neovim;
-          neovim = pkgs.neovim;
-        };
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import ./config/nvim.nix)
+        ];
+      };
+    in
+    {
+      packages = {
+        default = pkgs.neovim;
+        neovim = pkgs.neovim;
+      };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.neovim ];
-        };
-      }
-    );
+      devShells.default = pkgs.mkShell {
+        buildInputs = [ pkgs.neovim ];
+      };
+    }
+  );
 }
