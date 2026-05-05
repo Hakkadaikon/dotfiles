f768c1cd99fa49decb9fd26f626af09b1f2e7b18
hakkadaikon <hakkadaikon@yahoo.co.jp>
2026-05-06 05:04:21 +0900

    feat: Add set hl (NonText, Whitespace)

--- home/.config/nvim/lua/my/color.lua
+++ home/.config/nvim/lua/my/color.lua
@@ -6,6 +6,8 @@ function mycolor.setup()
   vim.opt.pumblend = 0
 
   vim.api.nvim_set_hl(0, "LineAdove", { fg = "#ffe5b4" })
+  vim.api.nvim_set_hl(0, "NonText", { fg = "#565f89" })
+  vim.api.nvim_set_hl(0, "Whitespace", { fg = "#565f89" })
 end
 
 return mycolor
